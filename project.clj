ad934c0e2e01e2d05332c2dbb387981d41489f32
Kyle Kingsbury <kingsbury@factual.com>
2013-11-27 18:48:52 -0800

    0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject meitner "0.1.0"
+(defproject meitner "0.1.1-SNAPSHOT"
   :description "Breaks apart macros/functions into dependency graphs"
   :url "http://github.com/aphyr/meitner"
   :license {:name "Eclipse Public License"
